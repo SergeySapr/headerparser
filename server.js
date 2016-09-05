bc96f1e07c626d989dac064be158a4c12fa79ee7
SergeySapr <sergey.activate@gmail.com>
2016-09-05 15:03:31 +0000

    Added language, IP and OS fields to the JSON response.

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+var app = express();
+    
+app.get('/',function(req,res){ 
+
+var userInfo={};
+userInfo.lang = req.headers['accept-language'].substr(0,5);
+userInfo.IP= req.headers['x-forwarded-for'];
+
+var userAgent= req.headers['user-agent'];
+var arrAgent = userAgent.split(")");
+userInfo.OS = arrAgent[0].match(/\((.+)/)[1];
+
+ res.end(JSON.stringify(userInfo));
+});
+var port = +process.env.PORT
+if (!port) port = 8080;
+
+app.listen(port, function () {console.log(process.env.PORT,process.env.IP);
+  console.log('User-info app listening on port 8080!');
+});
